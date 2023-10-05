eb14fc6ac0a0fd9e067317021fcaf2d2515f831b
jaba danelia <jabadanelia86@gmail.com>
2023-10-05 21:35:40 +0400

    Add calc.py

---
+++ calc.py
@@ -0,0 +1,17 @@
+print("Best calculator ever!")
+
+x = int(input("X: "))
+y = int(input("Y: "))
+
+
+
+print(f"sum: {x+y}")
+print(f"Sub: {x - y}")
+print(f"Mul: {x * y}")
+
+if y == 0:
+	print("can't do it")
+else:
+	print(f"Div: {x / y}")
+
+
